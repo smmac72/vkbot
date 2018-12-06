ebfb0c3fb75d1018840aaf52baf6bf27e06de2e7
Oleg <smmac072@gmail.com>
2018-12-06 10:19:19 +0300

    отложенный постинг

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
+import datetime
+import threading
 import time
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-def subUser():
+
+    
+def subUser(event):
     userID = event.obj.from_id # ИД юзера
     with open("people.txt") as ids:
         users = [row.strip() for row in ids]
@@ -30,7 +34,7 @@ def subUser():
         )
 
 
-def unsubUser():
+def unsubUser(event):
     userID = event.obj.from_id # ИД юзера
     with open("people.txt") as ids:
         users = [row.strip() for row in ids]
@@ -56,7 +60,7 @@ def unsubUser():
             message="Вы еще не подписаны!"
         )
 
-def subConversation():
+def subConversation(event):
     chatID = event.chat_id # ID чата
     with open("chats.txt") as ids:
         chats = [row.strip() for row in ids]
@@ -82,7 +86,7 @@ def subConversation():
             message="Ваша беседа уже подписана!"
         )
 
-def unsubConversation():
+def unsubConversation(event):
     chatID = event.chat_id # ID чата
     with open("chats.txt") as ids:
         chats = [row.strip() for row in ids]
@@ -107,12 +111,11 @@ def unsubConversation():
             message="Ваша беседа еще не подписана!"
         )
         
-def addToDelayed(att):
+def addToDelayed(event, att):
     send = event.obj.text # текст, введенный юзером
     if (len(send) > 5): # действие с сообщением с текстом
         # создание списков людей, которым откладыватся рассылки #
         date = send[6:22]
-        date[13] = '.'
         send = send[23:]
         if (len(send) != 0):
 
@@ -127,7 +130,7 @@ def addToDelayed(att):
             dt.write(text)
             dt.close()
 
-            name = "\\DelayedPosts\\" + date + ".txt"
+            name = str(len(dates)+1) + ".txt"
             delayed = open(name, mode = 'w')
 
             inp = ''
@@ -139,9 +142,111 @@ def addToDelayed(att):
         else: # отправка рассылки без текста
             if (len(att) != 0): # но с аттачментами
                 inp += 'att: ' + att
+        
+        delayed.write(inp)
+        delayed.close()
     return   
 
-def SendMessage():
+def PostDelayed():
+    longpoll = VkBotLongPoll(vk_session, '174143158')
+    while (True):
+        now = datetime.datetime.now()
+        with open("dates.txt") as ids:
+            dates = [row.strip() for row in ids]
+        for i in range(len(dates)):
+            day = int(dates[i][0] + dates[i][1])
+            month = int(dates[i][3] + dates[i][4])
+            year = int(dates[i][6] + dates[i][7] + dates[i][8] + dates[i][9])
+            hour = int(dates[i][11] + dates[i][12])
+            minute = int(dates[i][14] + dates[i][15])
+
+            curTime = time.ctime(time.time()).split()
+            curDay = int(curTime[2])
+            curMonth = curTime[1]
+            if curMonth == 'Jan':
+                curMonth = 1
+            elif curMonth == 'Feb':
+                curMonth = 2
+            elif curMonth == 'Mar':
+                curMonth = 3
+            elif curMonth == 'Apr':
+                curMonth = 4
+            elif curMonth == 'May':
+                curMonth = 5
+            elif curMonth == 'Jun':
+                curMonth = 6
+            elif curMonth == 'Jul':
+                curMonth = 7
+            elif curMonth == 'Aug':
+                curMonth = 8
+            elif curMonth == 'Sep':
+                curMonth = 9
+            elif curMonth == 'Oct':
+                curMonth = 10
+            elif curMonth == 'Nov':
+                curMonth = 11
+            elif curMonth == 'Dec':
+                curMonth = 12
+            curYear = int(curTime[4])
+            curTime = curTime[3]
+            curHour = int(curTime[0] + curTime[1])
+            curMinute = int(curTime[3] + curTime[4])
+            if (curYear == year and curMonth == month and curDay == day and curHour == hour and curMinute == minute):
+                with open(str(i+1) + ".txt") as ids:
+                    sends = [row.strip() for row in ids]
+                
+                with open("people.txt") as ids:
+                    users = [row.strip() for row in ids]
+                with open("chats.txt") as ids:
+                    chat = [row.strip() for row in ids]
+                if len(sends) != 1:
+                    for ids in users:
+                        vk.messages.send(
+                            user_id=ids,
+                            random_id = 0,
+                            message = sends[1][6:],
+                            attachment = sends[0][5:]
+                        )
+                    for ids in chat:
+                        vk.messages.send(
+                            chat_id=ids,
+                            random_id = 0,
+                            message = sends[1][6:],
+                            attachment = sends[0][5:]
+                        )
+                else:
+                    if (sends[0][0:3] == 'att'):
+                        for ids in users:
+                            vk.messages.send(
+                                user_id=ids,
+                                random_id = 0,
+                                attachment = sends[0][5:]
+                            )
+                        for ids in chat:
+                            vk.messages.send(
+                                chat_id=ids,
+                                random_id = 0,
+                                attachment = sends[0][5:]
+                            )
+                    elif(sends[0][0:4] == 'send'):
+                        for ids in users:
+                            vk.messages.send(
+                                user_id=ids,
+                                random_id = 0,
+                                message = sends[0][6:]
+                            )
+                        for ids in chat:
+                            vk.messages.send(
+                                chat_id=ids,
+                                random_id = 0,
+                                message = sends[0][6:]
+                            )
+                        
+                    
+
+        time.sleep(60)
+        
+def SendMessage(event):
     userID = event.obj.from_id # ИД юзера
     conv = vk.messages.getConversationsById(peer_ids = userID) # получение информации о диалоге
     lMes = conv.get('items')[0].get('last_message_id') # получение ID последнего сообщения в диалоге с пользователем
@@ -167,7 +272,7 @@ def SendMessage():
     send = event.obj.text # текст, введенный юзером
     if (len(send) > 11):
        if (send[8] == '.' or send[8] == '/' and send[11] == '.' or send[11] == '/' and send[19] == ':'):
-           addToDelayed(att)
+           addToDelayed(event, att)
            return
     if (len(send) > 6): # действие с сообщением с текстом
         # создание списков людей, которым отсылаются рассылки #
@@ -227,7 +332,7 @@ def SendMessage():
                 )
     return
 
-def isAdmin():
+def isAdmin(event):
     userID = event.obj.from_id # ИД юзера
     with open("admins.txt") as ids: # чек айдишников админов
         adms = [row.strip() for row in ids]
@@ -236,36 +341,49 @@ def isAdmin():
             sending = 1 # присвоение админки
     return (sending == 1)
 
-def ProcessCommand():
-    text = event.obj.text
-    if (text == '/sub'):
-        if (event.from_user):
-            subUser()
-        elif (event.from_chat):
-            subConversation()
-    elif (text == '/unsub'):
-        if (event.from_user):
-            unsubUser()
-        elif (event.from_chat):
-            unsubConversation()
-    elif (text[:5] == '/send'):
-        if (isAdmin()):
-            SendMessage()
-    return
 
+def Bot():
+    longpoll = VkBotLongPoll(vk_session, '174143158')
+    for event in longpoll.listen():
+        if (event.type == VkBotEventType.MESSAGE_NEW): # если новое сообщение
+            if (len(event.obj.text) != 0):
+                if (event.obj.text[0] == '/'):
+                    text = event.obj.text
+                    if (text == '/sub'):
+                        if (event.from_user):
+                            subUser(event)
+                        elif (event.from_chat):
+                            subConversation(event)
+                    elif (text == '/unsub'):
+                        if (event.from_user):
+                            unsubUser(event)
+                        elif (event.from_chat):
+                            unsubConversation(event)
+                    elif (text[:5] == '/send'):
+                        if (isAdmin(event)):
+                            SendMessage(event)
 
-####################### ВОТ ТУТ НАЧИНАЕТСЯ БОТ #######################
-### авторизация ###
 vk_session = vk_api.VkApi(token='212bfe399afff4b3ea2d0f12d16e2885fb92320f365f6fb514a56b6ae32f12de0456269c19df73e5503a1')
-longpoll = VkBotLongPoll(vk_session, '174143158')
+# longpoll = VkBotLongPoll(vk_session, '174143158')
 vk = vk_session.get_api()
-###################
 
-for event in longpoll.listen():
-    if (event.type == VkBotEventType.MESSAGE_NEW): # если новое сообщение
-        if (len(event.obj.text) != 0):
-            if (event.obj.text[0] == '/'):
-                ProcessCommand()
+post = threading.Event()
+postThread = threading.Thread(target=PostDelayed)
+
+bot = threading.Event()
+botThread= threading.Thread(target = Bot)
+
+postThread.start()
+botThread.start()
+
+post.set()
+
+postThread.join()
+botThread.join()
+
+
+
+
 
 ################# А ТУТ ОН КОНЧАЕТСЯ ##################################
 
